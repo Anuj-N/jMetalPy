6d44151e31479a18bc40a2ead6307ec0cc229d89
Anwesh Bhattacharya <anwesh0304@gmail.com>
2020-12-20 02:00:02 +0530

    Added chaotic initialisation to SMPSO

--- jmetal/util/generator.py
+++ jmetal/util/generator.py
@@ -1,4 +1,5 @@
 import copy
+from . import chaosGen as cg
 from abc import ABC, abstractmethod
 from typing import TypeVar, Generic, List
 
@@ -18,32 +19,51 @@ R = TypeVar('R')
 
 class Generator(Generic[R], ABC):
 
-    @abstractmethod
-    def new(self, problem: Problem) -> R:
-        pass
+	@abstractmethod
+	def new(self, problem: Problem) -> R:
+		pass
 
 
 class RandomGenerator(Generator):
 
-    def new(self, problem: Problem):
-        return problem.create_solution()
+	def new(self, problem: Problem):
+		return problem.create_solution()
+		
+class ChaoticGenerator(Generator):
 
+	def __init__ (self, Np):
+		self.Np, self.counter = Np, 0
+		self.cgen = None
+		self.cp = None
+
+	def new(self, problem: Problem):
+		if self.cgen is None :
+			self.cgen = cg.Lorenz((self.Np, problem.number_of_variables), gens=1)
+		if self.counter % self.Np == 0 :
+			self.cp = self.cgen.chaosPoints(1)
+			self.counter = 0
+				
+		ret = problem.create_blank_solution()
+		ret.variables = [l + (u-l)*z for l, u, z in zip(problem.lower_bound, problem.upper_bound, self.cp[self.counter])]
+		self.counter += 1
+		return ret
+			
 
 class InjectorGenerator(Generator):
 
-    def __init__(self, solutions: List[Solution]):
-        super(InjectorGenerator, self).__init__()
-        self.population = []
+	def __init__(self, solutions: List[Solution]):
+		super(InjectorGenerator, self).__init__()
+		self.population = []
 
-        for solution in solutions:
-            self.population.append(copy.deepcopy(solution))
+		for solution in solutions:
+			self.population.append(copy.deepcopy(solution))
 
-    def new(self, problem: Problem):
-        if len(self.population) > 0:
-            # If we have more solutions to inject, return one from the list
-            return self.population.pop()
-        else:
-            # Otherwise generate a new solution
-            solution = problem.create_solution()
+	def new(self, problem: Problem):
+		if len(self.population) > 0:
+			# If we have more solutions to inject, return one from the list
+			return self.population.pop()
+		else:
+			# Otherwise generate a new solution
+			solution = problem.create_solution()
 
-        return solution
+		return solution
